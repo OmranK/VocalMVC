fa1d19d049bf330a221803533b886226c5e34864
Felix Khoja <o.khoja93@gmail.com>
2020-01-12 13:46:18 -0500

    Initial Commit

---
+++ VocalMVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VocalMVC
+//
+//  Created by Omran Khoja on 1/12/20.
+//  Copyright © 2020 AcronDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
